08be30df9c8607bcc6d49e29149704bb8c8c235c
Carbonneau <David.Carbonneau@aecom.com>
2020-07-13 13:18:22 -0400

    asdfasdfasdf

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+//app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}!`);
